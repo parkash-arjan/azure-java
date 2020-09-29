5b109eb6d7ee51bab84ee3bef9fb76839bef684b
Parkash Arjan <Arjan.Parkash.Git@gmail.com>
2020-09-29 14:51:29 -0400

    added component scan

--- java-azure-rest-service/src/main/java/application/JavaAzureApplication.java
+++ java-azure-rest-service/src/main/java/application/JavaAzureApplication.java
@@ -2,8 +2,10 @@ package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.fibonacci.rs.controller"})
 public class JavaAzureApplication {
 
 	public static void main(String[] args) {
--- java-azure-rest-service/src/main/java/com/fibonacci/rs/controller/HelloAzureController.java
+++ java-azure-rest-service/src/main/java/com/fibonacci/rs/controller/HelloAzureController.java
@@ -2,8 +2,6 @@ package com.fibonacci.rs.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fibonacci.model.beans.HelloAzureBean;
@@ -11,10 +9,10 @@ import com.fibonacci.model.beans.HelloAzureBean;
 @RestController
 public class HelloAzureController {
 
-	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
-	//@GetMapping(path = "/hello-world")
+	//@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+	@GetMapping(path = "/azure")
 	public String helloWorld() {
-		return "Hello World-";
+		return "Hello World";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
