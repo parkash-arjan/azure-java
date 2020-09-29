ac22bf2ec589698cef0b6c7a046e9a446927f949
Parkash Arjan <Arjan.Parkash.Git@gmail.com>
2020-09-29 13:38:10 -0400

    initial add

---
+++ java-azure-rest-service/src/main/java/javaazure/javaazure/JavaAzureApplication.java
@@ -0,0 +1,13 @@
+package javaazure.javaazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAzureApplication.class, args);
+	}
+
+}
