e98a48241bcfe59bc262daa413d84cfdb5b7224f
Parkash Arjan <Arjan.Parkash.Git@gmail.com>
2020-09-29 13:57:36 -0400

    initial add

---
+++ purchase-service/src/test/java/purchaseservice/javaazure/JavaAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package purchaseservice.javaazure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
