2187a7d4f5130ff41cccd9b3e5d532d842e8896f
Parkash Arjan <Arjan.Parkash.Git@gmail.com>
2020-09-29 13:57:02 -0400

    initial add

---
+++ product-service/src/main/java/productservice/javaazure/JavaAzureApplication.java
@@ -0,0 +1,13 @@
+package productservice.javaazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAzureApplication.class, args);
+	}
+
+}
---
+++ product-service/src/test/java/productservice/javaazure/JavaAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package productservice.javaazure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
