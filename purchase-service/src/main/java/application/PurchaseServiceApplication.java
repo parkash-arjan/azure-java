a36290a8ff1528e0aa6efb808401ba6f696b3688
Parkash Arjan <Arjan.Parkash.Git@gmail.com>
2020-09-29 17:12:58 -0400

    purchase service added

--- purchase-service/src/main/java/application/PurchaseServiceApplication.java
+++ purchase-service/src/main/java/application/PurchaseServiceApplication.java
@@ -2,8 +2,10 @@ package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.purchase" })
 public class PurchaseServiceApplication {
 
 	public static void main(String[] args) {
